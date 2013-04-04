2a22fe25a69039cd356310f9cda66638cbd048f1
Dave Jungst <djungst@gmail.com>
2013-04-04 14:48:37 -0600

    Added images & modified header sass

--- lib/helpers/html.rb
+++ lib/helpers/html.rb
@@ -8,18 +8,43 @@ module Sinatra::HtmlHelpers
       <ul id="sdt_menu" class="sdt_menu">
         <li>
           <a href="#">
-            <img src="images/1.jpg" alt=""/>
+            <img src="images/ribs.jpeg" alt=""/>
             <span class="sdt_active"></span>
             <span class="sdt_wrap">
-              <span class="sdt_link">Portfolio</span>
-              <span class="sdt_descr">My work</span>
+              <span class="sdt_link">Home</span>
+              <span class="sdt_descr">About</span>
             </span>
           </a>
-          <div class="sdt_box">
-            <a href="#">Websites</a>
-            <a href="#">Illustrations</a>
-            <a href="#">Photography</a>
-          </div>
+        </li>
+        <li>
+         <a href="#">
+          <img src="images/fire.png" alt=""/>
+          <span class="sdt_active"></span>
+          <span class="sdt_wrap">
+            <span class="sdt_link">Photos</span>
+            <span class="sdt_descr">See our work</span>
+          </span>
+          </a>
+        </li>
+        <li>
+         <a href="#">
+          <img src="images/kisspig.jpg" alt=""/>
+          <span class="sdt_active"></span>
+          <span class="sdt_wrap">
+            <span class="sdt_link">Testimonials</span>
+            <span class="sdt_descr">Satisfied customers</span>
+         </span>
+          </a>
+        </li>
+        <li>
+         <a href="#">
+          <img src="images/pig1.jpg" alt=""/>
+          <span class="sdt_active"></span>
+          <span class="sdt_wrap">
+            <span class="sdt_link">Contact</span>
+            <span class="sdt_descr">Questions or comments?</span>
+          </span>
+          </a>
         </li>
       </ul>
     NAV
