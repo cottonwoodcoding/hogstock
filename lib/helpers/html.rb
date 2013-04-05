896c3d6abe67fd44e878dc8575694821ea24e993
Dave Jungst <djungst@gmail.com>
2013-04-05 16:10:34 -0600

    Added side menu

--- lib/helpers/html.rb
+++ lib/helpers/html.rb
@@ -2,6 +2,30 @@ module Sinatra::HtmlHelpers
   def h(str)
     Rack::Utils.escape_html(str)
   end
+  
+  def half_menu
+    <<-NAV
+      <ul id="sdt_menu" class="sdt_menu">
+        <li id="side_menu">
+          <a href="#" id="side_home">
+            <span class="sdt_active"></span>
+            <span class="sdt_wrap">
+              <span class="sdt_link">Main Menu</span>
+              <span class="sdt_descr"></span>
+            </span>
+          </a>
+          <div class="sdt_box">
+            <a href="#">Home</a>
+            <a href="#">Menu</a>
+            <a href="#">Photos</a>
+            <a href="#">Contact</a>
+            <a href="#">Testimonials</a>
+         </div>
+        </li>
+      </ul>
+    NAV
+  end
+
 
   def nav_menu
     <<-NAV
